cfa02ba50246308f907036784aa47f21eb014b14
pelmenos <vadimshvynda@gmail.com>
2021-12-11 19:05:51 +0700

    3

--- main.py
+++ main.py
@@ -2,14 +2,31 @@ import sys
 
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5 import uic
+from PyQt5 import QtCore, QtGui, QtWidgets
 import random
 
 
-class Example(QWidget):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(592, 430)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(220, 30, 121, 31))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Кнопка"))
+
+
+class Example(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.setWindowTitle('Рисование')
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
@@ -23,18 +40,20 @@ class Example(QWidget):
             qp.end()
 
     def draw_circ(self, qp):
-        x = random.randint(1, 400)
+        x = random.randint(1, 500)
         y = random.randint(1, 400)
-        r = random.randint(10, 100)
-        self.circle.append([x, y, r])
+        d = random.randint(10, 100)
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        self.circle.append([x, y, d, r, g, b])
         for i in self.circle:
-            qp.setBrush(QColor('yellow'))
+            qp.setBrush(QColor(i[3], i[4], i[5]))
             qp.drawEllipse(i[0], i[1], i[2], i[2])
 
     def paint(self):
         self.do_paint = True
         self.repaint()
-
 
 
 if __name__ == '__main__':
